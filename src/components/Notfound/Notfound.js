bf8d9fe6c515973ae39476519e46d121e8e76961
foadtkf <fakhrulif@gmail.com>
2022-05-06 11:33:17 +0600

    404 page added

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            
+            <img className='w-100' src="https://shots.codepen.io/djtms/pen/Exbdgob-1280.jpg?version=1646005869" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
