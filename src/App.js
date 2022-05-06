4b130d8fd6c1ffc32f046c2b333023436f8c247b
foadtkf <fakhrulif@gmail.com>
2022-05-06 11:48:45 +0600

    login system added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Services from './components/Services/Services';
 import NotFound from './components/Notfound/Notfound';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
       <Route path='/home' element={<Home></Home>}></Route>
       <Route path='/services' element={<Services></Services>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/signup' element={<Signup></Signup>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
